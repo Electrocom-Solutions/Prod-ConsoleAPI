957300f56f78740939d3d38439261bc20e6e5d67
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-11-11 19:07:38 +0530

    updated admin file for DeviceToken

--- Notifications/admin.py
+++ Notifications/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Notification, EmailTemplate
+from .models import Notification, EmailTemplate, DeviceToken
 
 
 @admin.register(Notification)
@@ -48,3 +48,28 @@ class EmailTemplateAdmin(admin.ModelAdmin):
             obj.created_by = request.user
         obj.updated_by = request.user
         super().save_model(request, obj, form, change)
+
+
+@admin.register(DeviceToken)
+class DeviceTokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'device_type', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('device_type', 'is_active', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'user__email', 'token', 'device_id')
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'updated_at', 'token_preview')
+    fieldsets = (
+        ('Device Information', {
+            'fields': ('user', 'token', 'token_preview', 'device_type', 'device_id', 'is_active')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+
+    def token_preview(self, obj):
+        """Show truncated token for display"""
+        if obj.token:
+            return f"{obj.token[:50]}..." if len(obj.token) > 50 else obj.token
+        return "-"
+    token_preview.short_description = "Token Preview"
